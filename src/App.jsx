e871f3c5983a2b9d240f4adee22a170310717281
Nipaaaa1 <Frostone224@gmail.com>
2024-01-12 10:25:12 +0700

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <p>We're coming soon</p>
+      <p>
+        Hello fellow shoppers! We're currently building our new fashion store.
+        Add your email below to stay up-to-date with announcements and our
+        launch deals.
+      </p>
+      <p>Email Address</p>
+    </>
+  );
+}
+
+export default App;
