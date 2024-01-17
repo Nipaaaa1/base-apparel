c6ab9c4c7de83969eb1ccc217da4f3cc79a5ee9d
Nipaaaa1 <Frostone224@gmail.com>
2024-01-17 17:11:12 +0700

    Desktop Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
 function App() {
   return (
     <>
-      <p>We're coming soon</p>
-      <p>
-        Hello fellow shoppers! We're currently building our new fashion store.
-        Add your email below to stay up-to-date with announcements and our
-        launch deals.
-      </p>
-      <p>Email Address</p>
+      <main className="relative flex h-screen w-screen bg-firstGradient">
+        <nav className="absolute left-56 top-16">
+          <img src="/images/logo.svg" alt="Logo" />
+        </nav>
+        <section className="md:bg-patternDesktop w-full space-y-10 bg-cover bg-center bg-no-repeat p-56">
+          <h1 className="tracking-heading text-8xl font-semibold uppercase text-darkGrayishRed">
+            <span className="font-light text-desaturatedRed">We're</span> coming
+            soon
+          </h1>
+          <p className="text-md w-4/5 text-pretty font-semibold tracking-wide text-desaturatedRed">
+            Hello fellow shoppers! We're currently building our new fashion
+            store. Add your email below to stay up-to-date with announcements
+            and our launch deals.
+          </p>
+          <form action="" className="relative w-4/5">
+            <input
+              className="w-full rounded-full border-2 bg-transparent px-6 py-4"
+              type="email"
+              placeholder="Email Address"
+            />
+            <button
+              type="submit"
+              className="absolute right-0 rounded-full bg-secondGradient px-12 py-5 shadow-xl shadow-softRed/30 transition ease-in-out hover:brightness-125"
+            >
+              <img src="/images/icon-arrow.svg" alt="Submit" />
+            </button>
+          </form>
+        </section>
+        <img className="h-screen w-max" src="/images/hero-desktop.jpg" alt="" />
+      </main>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        desaturatedRed: "hsl(0, 36%, 70%)",
+        softRed: "hsl(0, 93%, 68%)",
+        darkGrayishRed: "hsl(0, 6%, 24%)",
+      },
+      backgroundImage: {
+        firstGradient:
+          "linear-gradient(135deg, hsl(0, 0%, 100%), hsl(0, 100%, 98%))",
+        secondGradient:
+          "linear-gradient(135deg, hsl(0, 80%, 86%), hsl(0, 74%, 74%))",
+        heroDesktop: "url('/images/hero-desktop.jpg')",
+        patternDesktop: "url('/images/bg-pattern-desktop.svg')",
+      },
+      lineHeight: {
+        heading: "0.75",
+      },
+      letterSpacing: {
+        heading: "1.5rem",
+      },
+    },
   },
   plugins: [],
-}
+};
